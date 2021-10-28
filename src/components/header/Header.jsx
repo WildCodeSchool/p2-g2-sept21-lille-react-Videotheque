c52665648c936c50ca4414b1cea1fce604e7a0f3
BBriset <briset.b@gmail.com>
2021-10-28 16:15:15 +0200

    Adding a Burger Menu component for mobile devices, styling it, and make the all header responsive

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,23 @@
 import './Header.css';
 import React from 'react';
+import { bubble as Menu } from 'react-burger-menu';
 
 const Header = () => {
   return (
-    <nav className="navbar">
-      <div className="logo">Keep N</div>
-      <ul className="navlink">
-        <li className="watchpage">Watchlist</li>
-      </ul>
-    </nav>
+    <>
+      <Menu width="40%" className="menu">
+        <ul>
+          <li className=" menu-item">Watchlist</li>
+        </ul>
+      </Menu>
+
+      <nav className="navbar ">
+        <div className="logo">Keep N</div>
+        <ul className="navlink">
+          <li className="watchpage ">Watchlist</li>
+        </ul>
+      </nav>
+    </>
   );
 };
 export default Header;
