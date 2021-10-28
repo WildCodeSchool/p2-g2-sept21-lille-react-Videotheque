bc0219023c6fd9ca7114da03071adcb27755b562
Othmane Khiter <othmane.khiter@gmail.com>
2021-10-28 12:03:01 +0200

    Created the search bar and the filter button without behavior, only static with their style and responsive

---
+++ src/components/filterButton/FilterButton.jsx
@@ -0,0 +1,13 @@
+import './FilterButton.css';
+
+function FilterButton() {
+  return (
+    <div>
+      <button className="Button" type="button">
+        Advanced Search
+      </button>
+    </div>
+  );
+}
+
+export default FilterButton;
