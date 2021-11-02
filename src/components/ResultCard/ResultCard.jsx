37043a8676a54eff6d1cad02646aa6eb64f5b02c
BBriset <briset.b@gmail.com>
2021-11-02 22:18:07 +0100

    Styling resultCard conponent

---
+++ src/components/ResultCard/ResultCard.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './ResultCard.css';
+
+const ResultCard = (movie) => {
+  const { title, poster_path: posterPath } = movie;
+
+  return (
+    <div className="resultcard">
+      <div className="poster">
+        {posterPath ? (
+          <img
+            src={`https://image.tmdb.org/t/p/w220_and_h330_face${posterPath}`}
+            alt={`${title} Poster`}
+          />
+        ) : (
+          <img
+            src="https:/viaplaceholder.com/220x300?text=image+not+found"
+            alt={`${title} Poster`}
+          />
+        )}
+      </div>
+      <div className="info">
+        <h3 className="title">{title}</h3>
+      </div>
+    </div>
+  );
+};
+export default ResultCard;
--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,16 +1,49 @@
-import React from 'react';
+import Axios from 'axios';
+import React, { useState } from 'react';
+import ResultCard from '../ResultCard/ResultCard';
 import './Search.css';
 
-function Search() {
+const Search = () => {
+  const [querys, setQuerys] = useState('');
+  const [results, setResults] = useState([]);
+
+  const onChange = (e) => {
+    e.preventDefault();
+    setQuerys(e.target.value);
+    Axios.get(
+      `https://api.themoviedb.org/3/search/movie?api_key=599ded6f0fc3bcaee1882e83ae0d438a&language=en-US&page=1&include_adult=false&query=${e.target.value}`
+    )
+      .then((response) => response.data)
+      .then((data) => setResults(data.results))
+      .catch(() => {
+        console.error('Erreur API');
+        setResults([]);
+      });
+  };
+
   return (
-    <div className="Search">
-      <input
-        className="SearchBar"
-        type="search"
-        placeholder="Search your movie"
-      />
-    </div>
+    <>
+      <div className="Search">
+        <input
+          className="SearchBar"
+          type="search"
+          placeholder="Search your movie"
+          value={querys}
+          onChange={onChange}
+        />
+      </div>
+      {results.length > 0 && (
+        <ul className="results">
+          {results.map((movie) => (
+            <li key={movie.id}>
+              <ResultCard {...movie} />
+            </li>
+          ))}
+        </ul>
+      )}
+      ;
+    </>
   );
-}
+};
 
 export default Search;
