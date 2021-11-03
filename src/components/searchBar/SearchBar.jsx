c447badacb113efc84e397fffcec46f1f0210bc2
BBriset <briset.b@gmail.com>
2021-11-03 12:24:45 +0100

    Remove the console.error and fix the query(s) typo

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -4,19 +4,18 @@ import ResultCard from '../ResultCard/ResultCard';
 import './Search.css';
 
 const Search = () => {
-  const [querys, setQuerys] = useState('');
+  const [query, setQuery] = useState(['']);
   const [results, setResults] = useState([]);
 
   const onChange = (e) => {
     e.preventDefault();
-    setQuerys(e.target.value);
+    setQuery(e.target.value);
     Axios.get(
       `https://api.themoviedb.org/3/search/movie?api_key=599ded6f0fc3bcaee1882e83ae0d438a&language=en-US&page=1&include_adult=false&query=${e.target.value}`
     )
       .then((response) => response.data)
       .then((data) => setResults(data.results))
       .catch(() => {
-        console.error('Erreur API');
         setResults([]);
       });
   };
@@ -28,7 +27,7 @@ const Search = () => {
           className="SearchBar"
           type="search"
           placeholder="Search your movie"
-          value={querys}
+          value={query}
           onChange={onChange}
         />
       </div>
