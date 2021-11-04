9192b68bfae3af4c05d7836a00b394984f55b7ea
BBriset <briset.b@gmail.com>
2021-11-04 13:26:24 +0100

    Recoding resultCard for lighter code and good standard

--- src/components/ResultCard/ResultCard.jsx
+++ src/components/ResultCard/ResultCard.jsx
@@ -1,25 +1,16 @@
-import React from 'react';
 import './ResultCard.css';
 import { Link } from 'react-router-dom';
 
 const ResultCard = (movie) => {
   const { title, poster_path: posterPath } = movie;
+  const poster = posterPath
+    ? `https://image.tmdb.org/t/p/w220_and_h330_face${posterPath}`
+    : `https://via.placeholder.com/220x330?text={${title} Poster}`;
 
   return (
     <Link to="/" className="resultCard">
-      <div className="poster">
-        {posterPath ? (
-          <img
-            src={`https://image.tmdb.org/t/p/w220_and_h330_face${posterPath}`}
-            alt={`${title} Poster`}
-          />
-        ) : (
-          <img
-            src="https://via.placeholder.com/220x330?text=image+not+found"
-            alt={`${title} Poster`}
-          />
-        )}
-      </div>
+      <img className="poster" src={poster} alt={`${title} Poster`} />
+
       <div className="info">
         <h3 className="title">{title}</h3>
       </div>
