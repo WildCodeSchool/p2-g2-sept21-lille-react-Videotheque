f3ae3ff9f7a0696ae03f266e30a6e0f0b806a0f7
Quentin Macquart <macquart.quentin@gmail.com>
2021-11-16 14:29:40 +0100

    Add jsx files

--- src/components/FicheFilm/FicheFilm.jsx
+++ src/components/FicheFilm/FicheFilm.jsx
@@ -1,42 +1,41 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import './FicheFilm.css';
-import buttonTrailer from './play.png';
+import { useParams } from 'react-router-dom';
 import star1 from './star1.png';
 import star2 from './star2.png';
 import star3 from './star3.png';
 import star4 from './star4.png';
 import star5 from './star5.png';
-import buttonAdd from './add.png';
 
 export default function FicheFilm() {
-  const [titles, setTitles] = useState([]);
-  const [voteAverages, setVoteAverages] = useState([]);
-  const [numberVotes, setNumberVotes] = useState([]);
-  const [posters, setPosters] = useState([]);
-  const [backdrops, setBackdrops] = useState([]);
-  const [directors, setDirectors] = useState([]);
-  const [trailers, setTrailers] = useState([]);
-  const [overviews, setOverviews] = useState([]);
-  const [runtimes, setRuntimes] = useState([]);
-  const [releaseDates, setReleaseDates] = useState([]);
+  const [title, setTitle] = useState([]);
+  const [voteAverage, setVoteAverage] = useState([]);
+  const [numberVote, setNumberVote] = useState([]);
+  const [poster, setPoster] = useState([]);
+  const [backdrop, setBackdrop] = useState([]);
+  const [director, setDirector] = useState([]);
+  const [trailer, setTrailer] = useState([]);
+  const [overview, setOverview] = useState([]);
+  const [runtime, setRuntime] = useState([]);
+  const [releaseDate, setReleaseDate] = useState([]);
   const [actors, setActors] = useState([]);
   const [genres, setGenres] = useState([]);
-
+  const { id } = useParams();
   useEffect(() => {
     axios
       .get(
-        'https://api.themoviedb.org/3/movie/576845?api_key=599ded6f0fc3bcaee1882e83ae0d438a'
+        `https://api.themoviedb.org/3/movie/${id}?api_key=599ded6f0fc3bcaee1882e83ae0d438a`
       )
       .then(({ data }) => {
-        setTitles(data.original_title);
-        setVoteAverages(data.vote_average);
-        setNumberVotes(data.vote_count);
-        setPosters(data.poster_path);
-        setBackdrops(data.backdrop_path);
-        setOverviews(data.overview);
-        setRuntimes(data.runtime);
-        setReleaseDates(data.release_date);
+        setTitle(data.original_title);
+        setVoteAverage(data.vote_average);
+        setNumberVote(data.vote_count);
+        setPoster(data.poster_path);
+        setBackdrop(data.backdrop_path);
+        setOverview(data.overview);
+        setRuntime(data.runtime);
+        setReleaseDate(data.release_date);
         setGenres(data.genres);
       });
   }, []);
@@ -44,84 +43,80 @@ export default function FicheFilm() {
   useEffect(() => {
     axios
       .get(
-        'https://api.themoviedb.org/3/movie/576845/credits?api_key=599ded6f0fc3bcaee1882e83ae0d438a'
+        `https://api.themoviedb.org/3/movie/${id}/credits?api_key=599ded6f0fc3bcaee1882e83ae0d438a`
       )
       .then(({ data }) => {
-        setDirectors(data.crew[5]);
+        setDirector(data.crew[5]);
         setActors(data.cast);
+      })
+      .catch(() => {
+        console.error('Erreur API');
       });
   }, []);
 
   useEffect(() => {
     axios
       .get(
-        'https://api.themoviedb.org/3/movie/576845/videos?api_key=599ded6f0fc3bcaee1882e83ae0d438a'
+        `https://api.themoviedb.org/3/movie/${id}/videos?api_key=599ded6f0fc3bcaee1882e83ae0d438a`
       )
       .then(({ data }) => {
-        setTrailers(data.results[0]);
+        setTrailer(data.results[0]);
+      })
+      .catch(() => {
+        console.error('Erreur API');
       });
   }, []);
 
-  const Star = () => {
-    if (voteAverages === 10) {
-      return star5;
-    }
-    if (voteAverages < 10 && voteAverages >= 8) {
-      return star4;
-    }
-    if (voteAverages < 8 && voteAverages >= 6) {
-      return star3;
-    }
-    if (voteAverages < 6 && voteAverages >= 4) {
-      return star2;
-    }
-    return star1;
-  };
+  let UsersScorePictures;
+  if (voteAverage === 10) {
+    UsersScorePictures = star5;
+  } else if (voteAverage < 10 && voteAverage >= 8) {
+    UsersScorePictures = star4;
+  } else if (voteAverage < 8 && voteAverage >= 6) {
+    UsersScorePictures = star3;
+  } else if (voteAverage < 6 && voteAverage >= 4) {
+    UsersScorePictures = star2;
+  } else if (voteAverage < 4) {
+    UsersScorePictures = star1;
+  }
 
-  const hour = (runtimes - (runtimes % 60)) / 60;
-  const min = (runtimes / 60 - hour) * 60;
+  const hour = (runtime - (runtime % 60)) / 60;
+  const min = (runtime / 60 - hour) * 60;
 
   return (
-    <div>
+    <div className="filmMainBloc">
       <div className="titreVote">
-        <p>{titles}</p>
-        <div className="vote">
-          <img className="starScore" src={Star()} alt="StarScore" />
-          <div className="numberVote">{numberVotes} votes</div>
-        </div>
+        <p>{title}</p>
+        <p className="vote">
+          <img className="starScore" src={UsersScorePictures} alt="StarScore" />
+          <br />
+          <div className="numberVote">{numberVote} votes</div>
+        </p>
       </div>
       <div className="posterTrailer">
         <img
           className="backgroundPoster"
-          src={`https://image.tmdb.org/t/p/original${backdrops}`}
+          src={`https://image.tmdb.org/t/p/original${backdrop}`}
           alt="fond"
         />
+
+        <img
+          className="poster"
+          src={`https://image.tmdb.org/t/p/original${poster}`}
+          alt="trailer"
+        />
+
         <div className="directorTimeDate">
           <p>
-            {directors.job} :
+            {director.job} :
             <br />
-            {directors.name}
+            {director.name}
           </p>
           <p>
             {hour} H {min}
+            <br /> ({releaseDate})
           </p>
-          <p>({releaseDates})</p>
         </div>
-        <div className="buttons">
-          <img className="buttonAdd" src={buttonAdd} alt="addCollection" />
-          <a href={`https://www.youtube.com/embed/${trailers.key}`}>
-            <img
-              className="buttonTrailer"
-              src={buttonTrailer}
-              alt="playTrailer"
-            />
-          </a>
-        </div>
-        <img
-          className="poster"
-          src={`https://image.tmdb.org/t/p/original${posters}`}
-          alt="trailer"
-        />
 
         <div className="genresOverview">
           <div className="genres">
@@ -129,10 +124,19 @@ export default function FicheFilm() {
               return <p>{genre.name}</p>;
             })}
           </div>
-          <p className="overview">{overviews}</p>
+          <p className="overview">{overview}</p>
         </div>
       </div>
-
+      <div className="buttons">
+        <button type="button" className="buttonAdd">
+          +
+        </button>
+        <a href={`https://www.youtube.com/embed/${trailer.key}`}>
+          <div className="buttonTrailer" alt="Trailer">
+            <div className="playTriangle" />
+          </div>
+        </a>
+      </div>
       <div className="actors">
         {actors
           .filter((actor) => actor.order < 3)
