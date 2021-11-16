e1cddc6b565447e3f3842ec3fc61c933a76ae0d8
Quentin Macquart <macquart.quentin@gmail.com>
2021-11-16 14:20:57 +0100

    Delete files which was created by error

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,7 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Carousel from './components/carousel/Carousel';
-import SearchBar from './components/searchBar/SearchBar';
-import Header from './components/header/Header';
-import Footer from './components/footer/Footer';
-import Watchlist from './components/watchlist/Watchlist';
 import './App.css';
 
-import FicheFilm from './components/FicheFilm/FicheFilm';
-
 function App() {
-  return (
-    <div className="main">
-      <BrowserRouter>
-        <Switch>
-          <Route path="/" exact>
-            <Header />
-            <Carousel />
-            <SearchBar />
-            <Footer />
-          </Route>
-
-          <Route path="/FicheFilm/:id">
-            <Header />
-            <FicheFilm />
-          </Route>
-          <Route path="/Watchlist">
-            <Header />
-            <Watchlist />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
+  return <div className="main" />;
 }
 
 export default App;
--- src/components/Categories/Categories.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-const categories = () => {
-  return <div>hello</div>;
-};
-export default categories;
