f52d1ef76253e7bd4fbe59777c75eff8c6fcddc0
clairedelaleau <claire.delaleau@gmail.com>
2021-11-17 10:12:20 +0100

    Modification of icon.

---
+++ src/components/footer/Credits.jsx
@@ -0,0 +1,15 @@
+import { SiThemoviedatabase } from 'react-icons/si';
+
+const Credits = () => {
+  return (
+    <div className="wrapper">
+      <div className="icon credits">
+        <a href="https://www.themoviedb.org/" target="blank">
+          <SiThemoviedatabase />
+        </a>
+        <div className="toolTip">The Movie DataBase</div>
+      </div>
+    </div>
+  );
+};
+export default Credits;
