29f8a92ceabadbf10db5949ad995ec18815a38d2
clairedelaleau <claire.delaleau@gmail.com>
2021-11-18 14:59:55 +0100

    Add team component

---
+++ src/components/footer/Team.jsx
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom';
+import { RiTeamFill } from 'react-icons/ri';
+
+const Team = () => {
+  return (
+    <div className="wrapper">
+      <div className="icon github">
+        <Link to="./AboutUs" className="linkAboutUs" target="blank">
+          <RiTeamFill />
+        </Link>
+        <div className="toolTip">About Us</div>
+      </div>
+    </div>
+  );
+};
+
+export default Team;
