1e7228930a5961454b3fc1015b22050177de148d
BBriset <briset.b@gmail.com>
2021-11-18 13:20:52 +0100

    Fix .then inside searchBar, removing not use css

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -15,8 +15,9 @@ const SearchBar = () => {
     Axios.get(
       `https://api.themoviedb.org/3/search/movie?api_key=599ded6f0fc3bcaee1882e83ae0d438a&language=en-US&page=1&include_adult=false&query=${e.target.value}`
     )
-      .then((response) => response.data)
-      .then((data) => setResults(data.results))
+      .then(({ data }) => {
+        setResults(data.results);
+      })
       .catch(() => {
         setResults([]);
       });
