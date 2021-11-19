646ddd5e2106499930b4bd3feb5aff2d938aba2e
Quentin Macquart <macquart.quentin@gmail.com>
2021-11-19 11:45:00 +0100

    Update the file to add providers in local storage

--- src/components/FicheFilm/FicheFilm.jsx
+++ src/components/FicheFilm/FicheFilm.jsx
@@ -22,6 +22,7 @@ export default function FicheFilm() {
   const [actors, setActors] = useState([]);
   const [genres, setGenres] = useState([]);
   const [providersFlatrates, setProvidersFlatrates] = useState([]);
+  const [myProviders, setMyProviders] = useState([]);
   const { id } = useParams();
 
   const [watchlist, setWatchlist] = useState([]);
@@ -104,6 +105,20 @@ export default function FicheFilm() {
   };
 
   useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/${id}/watch/providers?api_key=599ded6f0fc3bcaee1882e83ae0d438a`
+      )
+
+      .then(({ data }) => {
+        setMyProviders(data.results.FR.flatrate);
+      })
+      .catch(() => {
+        setMyProviders([]);
+      });
+  }, []);
+
+  useEffect(() => {
     const newList = JSON.parse(localStorage.getItem('watchlist'));
     if (newList) setWatchlist(newList);
   }, []);
@@ -113,6 +128,8 @@ export default function FicheFilm() {
       Id: id,
       Title: titles,
       PosterPath: `https://image.tmdb.org/t/p/original${backdrops}`,
+      Provider1: myProviders[0]?.logo_path,
+      Provider2: myProviders[1]?.logo_path,
     };
     const newWList = [...watchlist, foundMovie];
     setWatchlist(newWList);
